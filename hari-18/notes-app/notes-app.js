6f636724f8ded961f2e27038d3582bc5efcf5562
Mohamad Handy Nugraha <handynugraha29@gmail.com>
2019-04-28 07:06:04 +0000

    menambahkan index.html dan notes-app.js. Belajar DOM manipulation, dan install live-server di c9 tadi

---
+++ hari-18/notes-app/notes-app.js
@@ -0,0 +1,26 @@
+// dom
+// Document Object Model
+const h2 = document.querySelector("h2");
+h2.remove();
+
+// jadi konsep sederhana untuk DOM manipulation adalah
+// 1. Query dulu elementnya yang di html
+// 2. Element yang udah di query selanjutnya bisa diolah
+
+// nah sekarang di file index.html ada 2 paragraf.
+// kalau kodingan ini dijalankan
+const p = document.querySelector("p");
+console.log(p);
+// maka variable p akan menjadi "paragraph tidak dihapus" p tag yang pertama
+
+// coba dapatkan variable p semua
+const ps = document.querySelectorAll("p");
+console.log(ps);
+
+// hapus semua p tag dengan remove
+ps.forEach(function (item){
+    // item.remove(); // untuk remove
+    // console.log(item.textContent) // untuk mendapatkan text content dari element
+    item.textContent = "Paragraf yang sangat baru textnya";
+    
+});
